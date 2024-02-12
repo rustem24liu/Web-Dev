e588cc1296cfea5131a2e938281cefeb028e732e
rustem24liu <temirgalirustem05@gmail.com>
2024-02-12 18:40:28 +0600

    finished 3'rd lab

---
+++ lab3/todolist/static/js/script.js
@@ -0,0 +1,80 @@
+let addMessage = document.querySelector('.input'),
+    addButton = document.querySelector('.add'),
+    todo = document.querySelector('.todo');
+
+let todoList = []
+
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    displayMessages();
+}
+    
+addButton.addEventListener('click', function(){
+
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false,
+    };
+    
+    todoList.push(newTodo);
+    displayMessages();
+    localStorage.setItem('todo', JSON.stringify(todoList));
+});
+
+function displayMessages(){
+
+    let displayMessage = ``;
+
+    todoList.forEach(function(item, index){
+        displayMessage += `
+
+        <li>
+
+            <input type = 'checkbox' id='item_${index}' ${item.checked ? 'checked':''}>
+            <label for = 'item_${index}' class="${item.important ? 'important' : ''}">${item.todo}</label>
+            <img src = "static/img/delete.png" alt = "delete" style = "height: 15px" class = "delete" data-index="${index}">
+
+        </li>
+
+        `;
+
+        todo.innerHTML = displayMessage;
+        
+    })
+
+}
+
+todo.addEventListener('change', function(event){
+    
+    let valueLabel = todo.querySelector('[for=' + event.target.getAttribute('id') + ']').innerHTML;
+
+    todoList.forEach(function(item){
+
+        if(item.todo === valueLabel){
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList))
+        }
+        else{
+            console.log("mamammmm")
+        }
+    })
+
+});
+
+todo.addEventListener('click', function(event){
+
+    // event.preventDefault();
+    if(event.target.classList.contains('delete')){
+        const index = event.target.getAttribute('data-index');
+        todoList.splice(index, 1);
+        displayMessages();
+        localStorage.setItem('todo', JSON.stringify(todoList))
+        event.target.parentNode.remove(); 
+        // return;
+    }
+    else{
+        console.log("somethin ing th")
+    }
+
+});
