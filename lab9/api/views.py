618d6b64b45893e0c74d49f2973a0b7843cd76f7
rustem24liu <temirgalirustem05@gmail.com>
2024-04-09 20:36:07 +0500

    Lab 9

---
+++ lab9/api/views.py
@@ -0,0 +1,39 @@
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
+
+
+# Create your views here.
+
+
+def companies_list(request):
+    companies = Company.objects.all()
+    data = {'companies': list(companies.values())}
+
+    return JsonResponse(data)
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+    def vacancies(self, request, pk=None):
+        company = self.get_object()
+        vacancies = company.vacancies.all()
+        serializer = VacancySerializer(vacancies, many=True)
+        return Response(serializer.data)
+
+
+class VacancyViewSet(viewsets.ModelViewSet):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+    def top_ten(self, request, pk=None):
+        vacancies = Vacancy.objects.order_by('-salary')[:10]
+        serializer = VacancySerializer(vacancies, many=True)
+        return Response(serializer.data)
